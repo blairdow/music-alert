df0ebedc1fd6b00fa0ee979f74e88e938b17759a
blair dowis <blairdow@gmail.com>
2016-11-01 15:21:25 -0700

    updated seeds w streaming service

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,18 +9,21 @@
 User.delete_all
 Medium.delete_all
 
-blair = User.create({first_name: 'Blair', last_name: "Dowis", email: "blairdow@gmail.com", password: "abc123"})
-nevil = User.create({first_name: 'Nevil', last_name: "Jackson", email: "nevil.jackson@gmail.com", password: "abc123"})
-    
+blair = User.create({first_name: 'Blair', last_name: "Dowis", email: "blairdow@gmail.com", password: "abc123", streaming_service_url: 'https://play.spotify.com/browse'})
+nevil = User.create({first_name: 'Nevil', last_name: "Jackson", email: "nevil.jackson@gmail.com", password: "abc123", streaming_service_url: 'https://play.spotify.com/browse'})
+
     #users
-    100.times do 
+    102.times do 
+        services = ['https://play.spotify.com/browse', 'https://listen.tidal.com/']
+        num = Faker::Number.between(0, 1)
         User.create(
             first_name: Faker::Name.first_name,
             last_name: Faker::Name.last_name,
             email: Faker::Internet.email,
             password: 'abc123', 
             morning_commute: Faker::Time.forward(2, :morning), 
-            evening_commute: Faker::Time.forward(2, :evening)
+            evening_commute: Faker::Time.forward(2, :evening),
+            streaming_service_url: services[num]
             )
     end
     
