9eb0fd10fa3cc81b0077d248fcc1a89c968e95c0
blair dowis <blairdow@gmail.com>
2016-11-02 15:51:16 -0700

    heroku fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-    before_filter :set_user_time_zone
+#    before_filter :set_user_time_zone
+    
     def authorize
       redirect_to signup_path, alert: 'You have to sign up or log in to do that!' if current_user.nil?
     end
