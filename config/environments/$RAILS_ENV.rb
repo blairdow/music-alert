529af4dec681b4c2b6cda8244544632b570b600e
blair dowis <blairdow@gmail.com>
2017-02-09 09:56:50 -0800

    env variable

--- config/environments/$RAILS_ENV.rb
+++ config/environments/$RAILS_ENV.rb
@@ -9,4 +9,19 @@ config.action_mailer.delivery_method = :smtp
          :authentication       => "plain",
         :enable_starttls_auto => true
         }
+    
+config.cache_store = :redis_store, "redis://localhost:6379/1/ns"
+
+config.paperclip_defaults = {
+    storage: :s3,
+    s3_protocol: :https,
+    :s3_host_name => 's3-us-west-1.amazonaws.com',
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
+    }
+  }
+    
 end    
